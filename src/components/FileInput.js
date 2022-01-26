3767757db2273464513d1a05ad64112556001224
lilly0305 <yhklilly0305@gmail.com>
2022-01-26 19:04:45 +0900

    ~22-01-26

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,9 +1,45 @@
+import { useState, useRef, useEffect } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState();
+
+  useEffect(() => {
+    if(!value) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    }
+
+  }, [value]);
+
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   }
-  return <input type="file" onChange={handleChange}/>;
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+
+    if(!inputNode) return;
+
+    inputNode.value = '';
+    onChange(name, null);
+  }
+
+  return (
+    <div>
+      <img src={preview} alt="이미지 미리보기" />
+      <input type="file" onChange={handleChange} accept="image/png, image/jpeg" ref={inputRef}/>
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  )
+
 }
 
 export default FileInput;
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 import './ReviewForm.css';
 
 function ReviewForm() {
@@ -31,7 +32,7 @@ function ReviewForm() {
     <form className="ReviewForm" onSubmit={handleSubmit}>
       <input name="title" value={values.title} onChange={handleInputChange}></input>
       <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
-      <input  name="rating" type="number" value={values.rating} onChange={handleInputChange} />
+      <RatingInput name='rating' value={values.rating} onChange={handleChange} />
       <textarea  name="content" value={values.content} onChange={handleInputChange} />
       <button type="submit">확인</button>
     </form>
