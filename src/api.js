18cf8fb662ffdad61176ab78200d125c31633c91
lilly0305 <yhklilly0305@gmail.com>
2022-02-04 18:44:23 +0900

    데이터보내기 01. 글 작성하기 강의 끝

--- src/api.js
+++ src/api.js
@@ -1,3 +1,5 @@
+const BASE_URL = 'https://learn.codeit.kr/api';
+
 export async function getReviews({
   order = 'createdAt',
   offset = 0,
@@ -5,11 +7,23 @@ export async function getReviews({
 }) {
 
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`);
+  const response = await fetch(`${BASE_URL}/film-reviews?${query}`);
   if (!response.ok) {
     throw new Error('리뷰를 불러오는 데 실패했습니다.')
   }
   const body = await response.json();
   
+  return body;
+}
+
+export async function createReview(formData) {
+  const response = await fetch(`${BASE_URL}/film-reviews`,{
+    method: 'POST',
+    body: formData,
+  });
+  if (!response.ok) {
+    throw new Error('리뷰를 생성하는 데 실패했습니다.')
+  }
+  const body = await response.json();
   return body;
 }
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import { createReview } from "../api";
 import FileInput from "./FileInput";
 import RatingInput from "./RatingInput";
 import './ReviewForm.css';
 
+const INITIAL_VALUES = {
+  title: '',
+  rating: 0,
+  content: '',
+  imgFile: null,
+}
+
 function ReviewForm() {
-  const [values, setValues] = useState({
-    title: '',
-    rating: 0,
-    content: '',
-    imgFile: null,
-  })
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
+  const [values, setValues] = useState(INITIAL_VALUES)
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -23,10 +28,29 @@ function ReviewForm() {
     handleChange(name, value);
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append('title', values.title);
+    formData.append('rating', values.rating);
+    formData.append('content', values.content);
+    formData.append('imgFile', values.imgFile);
+
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      await createReview(formData);
+    }catch (error){
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+
     console.log(values);
-  }
+
+    setValues(INITIAL_VALUES);
+  };
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
@@ -34,7 +58,9 @@ function ReviewForm() {
       <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
       <RatingInput name='rating' value={values.rating} onChange={handleChange} />
       <textarea  name="content" value={values.content} onChange={handleInputChange} />
-      <button type="submit">확인</button>
+      <button type="submit" disabled={isSubmitting}>확인</button>
+
+      {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   )
 }
